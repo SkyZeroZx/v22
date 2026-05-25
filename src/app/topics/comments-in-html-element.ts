fef562772eb8e4ac189daf22085b84919fcc5f11
Michael Small <michaelsmalldev@gmail.com>
2026-05-24 20:26:13 -0500

    chore: link to comments in html PR for now

--- src/app/topics/comments-in-html-element.ts
+++ src/app/topics/comments-in-html-element.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-comments-in-html-element',
-  template: ` <p>TODO - comments in HTML element</p> `,
+  template: `
+    <p>TODO - update with PR's example once language service supports this officially</p>
+    <p>
+      <a href="https://github.com/angular/angular/pull/67179" target="_blank"
+        >PR feat(compiler): Support comments in html elements</a
+      >
+    </p>
+  `,
 })
 export class CommentsInHtmlElement {}
